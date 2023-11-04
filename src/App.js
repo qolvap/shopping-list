63cd9f1d4f4158d732355267423b8488dd02c994
qolvap <pgrebennikow@yahoo.pl>
2023-11-04 13:02:02 +0100

    🔄added styles, apdated the adding item fn, corrected the naming

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
@@ -8,19 +8,27 @@ const App = () => {
   const [inputValue, setInputValue] = useState("");
   const [totalItemCount, setTotalItemCount] = useState(0);
 
+  // handleAddBtn() fn handles the adding item to the arr of items
   const handleAddBtn = () => {
-    const newItem = {
-      itemNAme: inputValue,
-      quanity: 1,
-      isSelected: false,
-    };
-    const newItems = [...items, newItem]; // copy the state items & add new item to it
-    setItems(newItems); // updating the state with the new items
-    setInputValue(""); // clears the input field
-    setTotalItemCount(totalItemCount + 1); 
-  }
+    if (inputValue.trim() !== '') { 
+// checking if something was typed in the input section
+// .trim() deletes spaces from the begining & end of the 
+// inputValue & checks if after deliting the spaces inputValue isnt empty
+      const newItem = {
+        itemName: inputValue, 
+        quanity: 1,
+        isSelected: false,
+      };
+      const newItems = [...items, newItem]; // copy the state items & add new item to it
+      setItems(newItems); // updating the state with the new items
+      setInputValue(''); // clears the input field
+      setTotalItemCount(totalItemCount + 1);
+    } else {
+      alert('Please enter an item before adding.');
+    } // result of the fn is true if inputValue has any text & false if the inputValue is an empty string or only spaces
+  };
 
-
+  // handleQuantityIncrease() fn increase the amount of items
   const handleQuantityIncrease = (index) => { // each time the btn is clicked fn gets called we pass the index, which come from the .map fn which iteretes over arr & by index takes the position we currently on
     const newItems = [...items];
     newItems[index].quanity++; // takes the newItems arr by its index & increments the quantity
@@ -28,6 +36,7 @@ const App = () => {
     calculateTotal(); // calls the calculateTotal() to update the current total items value
   }
 
+    // handleQuantityDecrease() fn decrease the amount of items
   const handleQuantityDecrease = (index) => { // each time the btn is clicked fn gets called we pass the index, which come from the .map fn which iteretes over arr & by index takes the position we currently on
     const newItems = [...items];
     if (newItems[index].quanity > 0) {
@@ -36,6 +45,7 @@ const App = () => {
     calculateTotal(); // calls the calculateTotal() to update the current total items value
   }}
 
+  // toggleComplete() fn handles the checking if item was completed by taken the index of the item
   const toggleComplete = (index) => {
     const newItems = [...items];
     newItems[index].isSelected = !newItems[index].isSelected; // set isSelected to opposite what it currently is
@@ -51,16 +61,22 @@ const App = () => {
     const totalItemsCount = items.reduce((total, item) => {
       return total + item.quanity;
     }, 0); 
-
     setTotalItemCount(totalItemsCount) // update the totalItemCount state to what .reduce returns
+  }
+
+  // handleKeyPress is a fn that takes an event to call the handleAddBtn(), when the user presses 'Enter' keyboard
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleAddBtn(); // call the fn to add the item from the input by pressing the "Enter" key
+     }
   }
 
 	return (
 		<div className='app-background'>
 			<div className='main-container'>
 				<div className='add-item-box'>
-					<input value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='add-item-input' placeholder='Add an item...' />
-					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddBtn()} />
+					<input value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyPress={handleKeyPress} className='add-item-input' placeholder='Add an item...' />
+					<FontAwesomeIcon className="add-item-btn" icon={faPlus} onClick={() => handleAddBtn()} />
 				</div>
 				<div className='item-list'>
           {items.map((item, index) => <div className='item-container'>
@@ -68,12 +84,12 @@ const App = () => {
 							{item.isSelected ? (
 								<>
 									<FontAwesomeIcon icon={faCheckCircle} />
-									<span className='completed'>{item.itemNAme}</span>
+									<span className='completed'>{item.itemName}</span>
 								</>
 							) : (
 								<>
 									<FontAwesomeIcon icon={faCircle} />
-									<span>{item.itemNAme}</span>
+									<span>{item.itemName}</span>
 								</>
 							)}
 						</div>
